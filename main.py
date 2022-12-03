498769784a4080bd13f889f75d63517b06737781
adarshvmanu <adarshvmanu@gmail.com>
2022-12-03 19:09:18 +0530

    Second Commit

--- main.py
+++ main.py
@@ -49,17 +49,14 @@ class App(customtkinter.CTk):
         self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.home_frame.grid_columnconfigure(0, weight=1)
 
-        self.home_frame_large_image_label = customtkinter.CTkLabel(self.home_frame, text="", image=self.large_test_image)
-        self.home_frame_large_image_label.grid(row=0, column=0, padx=20, pady=10)
+        self.textbox = customtkinter.CTkTextbox(self.home_frame, width=500)
+        self.textbox.grid(row=0, column=2, padx=10, pady=10, sticky="ew")
 
-        self.home_frame_button_1 = customtkinter.CTkButton(self.home_frame, text="", image=self.image_icon_image)
-        self.home_frame_button_1.grid(row=1, column=0, padx=20, pady=10)
-        self.home_frame_button_2 = customtkinter.CTkButton(self.home_frame, text="CTkButton", image=self.image_icon_image, compound="left")
-        self.home_frame_button_2.grid(row=2, column=0, padx=20, pady=10)
-        self.home_frame_button_3 = customtkinter.CTkButton(self.home_frame, text="CTkButton", image=self.image_icon_image, compound="top")
-        self.home_frame_button_3.grid(row=3, column=0, padx=20, pady=10)
-        self.home_frame_button_4 = customtkinter.CTkButton(self.home_frame, text="CTkButton", image=self.image_icon_image, compound="bottom", anchor="w")
-        self.home_frame_button_4.grid(row=4, column=0, padx=20, pady=10)
+        self.entry = customtkinter.CTkEntry(self.home_frame, placeholder_text="Enter Task")
+        self.entry.grid(row=1, column=2, padx=(20, 20), pady=(20, 20), sticky="nsew")
+
+        self.home_frame_button_2 = customtkinter.CTkButton(self.home_frame, text="Add", image=self.image_icon_image, compound="left")
+        self.home_frame_button_2.grid(row=1, column=2, padx=20, pady=10)
 
         # select default frame
         self.select_frame_by_name("home")
